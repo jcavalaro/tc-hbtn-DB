1e9ae38fc1ebf0a1b30f14bffd3f4fe698744878
jcavalaro <jcavalaro@ciandt.com>
2022-06-23 11:04:51 -0300

    task4

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE(
+id INTEGER not null primary key autoincrement,
+nome varchar(80) not null,
+email varchar(40) not null,
+telefone INT
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
