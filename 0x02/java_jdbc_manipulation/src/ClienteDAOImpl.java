ea99844e525b19ebe986648423f1bd809d6d3dd6
jcavalaro <jcavalaro@ciandt.com>
2022-06-27 17:12:59 -0300

    task1

--- 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
+++ 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
@@ -1,34 +1,108 @@
-import java.sql.Connection;
+import java.sql.*;
 
 public class ClienteDAOImpl implements ClienteDAO {
 
     @Override
     public Connection connect(String urlConexao) {
-        return null;
+
+        Connection conn = null;
+       // Class.forName(urlConexao);
+        try {
+            //String url = "jdbc:sqlite:sqlite_database_2022.db";
+            conn = DriverManager.getConnection(urlConexao);
+            //System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
 
     @Override
     public void createTable(String urlConexao) {
+
+        String url = urlConexao;
+        StringBuffer sql = new StringBuffer();
+        sql.append("CREATE TABLE IF NOT EXISTS cliente (");
+        sql.append("id integer PRIMARY KEY , ");
+        sql.append("nome text NOT NULL, ");
+        sql.append("idade integer, ");
+        sql.append("cpf text , ");
+        sql.append("rg text ");
+        sql.append(")");
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql.toString());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
     @Override
     public void insert(String url_conexao, Cliente cliente) {
 
+        String sql = "INSERT INTO cliente(nome,idade, cpf, rg) VALUES(?,?,?,?)";
+        try (Connection conn = this.connect(url_conexao);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, cliente.getNome());
+            pstmt.setInt(2, cliente.getIdade());
+            pstmt.setString(3, cliente.getCpf());
+            pstmt.setString(4, cliente.getRg());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     @Override
     public void selectAll(String urlConexao) {
 
+        String sql = "SELECT id, nome, idade, cpf, rg FROM cliente";
+        try (Connection conn = this.connect(urlConexao);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" + rs.getString("nome") + "\t" + rs.getInt("idade")
+                                + "\t" +
+                        rs.getString("cpf") + "\t" + rs.getString("rg"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     @Override
-    public void update(String urlConexao, int id, String name, Integer idade) {
+    public void update(String urlConexao, Integer id, String name, Integer idade) {
+
+        String sql = "UPDATE cliente SET nome = ?," + "idade = ? " + "WHERE id = ?";
+        try (Connection conn = this.connect(urlConexao);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setInt(2, idade);
+            pstmt.setInt(3, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
     @Override
     public void delete(String urlConexao, int id) {
+        String sql = "DELETE FROM cliente WHERE id = ?";
 
+        try (Connection conn = this.connect(urlConexao);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the corresponding param
+            pstmt.setInt(1, id);
+            // execute the delete statement
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
